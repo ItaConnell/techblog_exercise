462e819ca2e75d27177feab4b988f79b0e7ae0f5
pporschen <accounts@pp83.de>
2020-05-13 17:01:07 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import * as contentful from 'contentful'
+
+const client = contentful.createClient({
+  space: '2qe6xgfwyxeo',
+  accessToken: '1QCB1pBKDFHSOQCxVAscDr5FxGa1JqP4eNXdjrcf0WI'
+})
 
 function App() {
+  const [content, setContent] = useState([])
+
+  useEffect(() => {
+    client.getEntries().then(entries => {
+      console.log(entries)
+
+    })
+  }, []
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {content.map(entry => {
+        return (
+          <div className="blogpost">
+            <h2>{entry.fields.title}</h2>
+            <p>{entry.fields.content.content[0].content[0].value}</p>
+          </div>
+        )
+      })}
+    </>
   );
 }
 
