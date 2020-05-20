721fbd11e6a250de78df84b3496799393974c8ba
Matthias <thomas.ahrens@me.com>
2020-05-20 15:36:51 +0200

    navbar 1.0

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const client = contentful.createClient({
 
 console.log(process.env.REACT_APP_API_KEY)
 
+
+
 function App() {
   const [content, setContent] = useState([])
 
@@ -22,12 +24,37 @@ function App() {
 
   return (
     <>
+          <div id="content-containter">
+            <nav class="navbar navbar-inverse">
+              <div class="container-fluid">
+                <div class="navbar-header">
+                  <div class="navbar-brand">TechnoNerds</div>
+                </div>
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Team</a></li>
+                    <li><a href="#">Sort By</a></li>
+                  </ul>
+                  <form class="navbar-form navbar-left" action="/action_page.php">
+                    <div class="input-group">
+                      <input type="text" class="form-control" placeholder="Search" name="search"></input>
+                      <div class="input-group-btn">
+                        <button class="btn btn-default" type="submit">
+                          <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+              </div>
+            </nav>
+          </div>
+
       {content.map(entry => {
         return (
-          <div className="blogpost">
-            <h2>{entry.fields.commentAuthor}</h2>
-            <p>{entry.fields.commentAuthorEmail}</p>
-          </div>
+            <div className="blogpost">
+              <h2>{entry.fields.commentAuthor}</h2>
+              <p>{entry.fields.commentAuthorEmail}</p>
+            </div>
         )
       })}
     </>
