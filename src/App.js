a4282b54280ffd2c01c7c445a3b32626ee09d326
Matthias <thomas.ahrens@me.com>
2020-05-20 17:51:33 +0200

    navbar 2.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import * as contentful from 'contentful'
+import Navbar from "./Navbar"
 
 const client = contentful.createClient({
   space: process.env.REACT_APP_SPACE_ID,
@@ -24,31 +25,7 @@ function App() {
 
   return (
     <>
-          <div id="content-containter">
-            <nav class="navbar navbar-inverse">
-              <div class="container-fluid">
-                <div class="navbar-header">
-                  <div class="navbar-brand">TechnoNerds</div>
-                </div>
-                  <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">Team</a></li>
-                    <li><a href="#">Sort By</a></li>
-                  </ul>
-                  <form class="navbar-form navbar-left" action="/action_page.php">
-                    <div class="input-group">
-                      <input type="text" class="form-control" placeholder="Search" name="search"></input>
-                      <div class="input-group-btn">
-                        <button class="btn btn-default" type="submit">
-                          <i class="glyphicon glyphicon-search"></i>
-                        </button>
-                      </div>
-                    </div>
-                  </form>
-              </div>
-            </nav>
-          </div>
-
+      <Navbar/>
       {content.map(entry => {
         return (
             <div className="blogpost">
